730f8d831ee9407fd7bb0c57e01011c245d02110
Roti18 <akuronif@gmail.com>
2025-12-24 01:03:33 +0700

    feat: reusable Input component

---
+++ src/lib/components/ui/Input.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let value: string | number;
+	export let type: string = 'text';
+	export let placeholder: string = '';
+	export let required: boolean = false;
+	export let id: string = '';
+	export let name: string = '';
+
+	let className: string = '';
+	export { className as class };
+</script>
+
+<input
+	{type}
+	{id}
+	{name}
+	bind:value
+	{placeholder}
+	{required}
+	class="w-full rounded-lg border border-zinc-800 bg-zinc-900 px-4 py-3 text-white placeholder-zinc-600 focus:border-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 {className}"
+	{...$$restProps}
+/>
