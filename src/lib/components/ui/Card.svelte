c96b3c3177464e6742621828b97a08c05ed48497
Roti18 <akuronif@gmail.com>
2025-12-24 01:03:25 +0700

    feat: reusable Card component

---
+++ src/lib/components/ui/Card.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let className: string = '';
+	export { className as class };
+</script>
+
+<div class="rounded-lg border border-zinc-800 bg-zinc-950 p-6 {className}">
+	<slot />
+</div>
