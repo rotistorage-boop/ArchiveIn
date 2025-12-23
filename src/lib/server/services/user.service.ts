c109412cb397feb30aacb8ab6fede731e4dd404f
Roti18 <akuronif@gmail.com>
2025-12-24 01:02:06 +0700

    refactor: user service

--- src/lib/server/services/user.service.ts
+++ src/lib/server/services/user.service.ts
@@ -1,4 +1,6 @@
 import { db } from '$lib/server/db';
+import { user } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export async function getUsers() {
 	const users = await db.query.user.findMany();
@@ -6,4 +8,8 @@ export async function getUsers() {
 		...user,
 		lastLogin: new Date().toLocaleString()
 	}));
+}
+
+export async function updateUserRole(id: string, role: string) {
+	await db.update(user).set({ role }).where(eq(user.id, id));
 }
